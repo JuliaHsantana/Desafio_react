def114b431f3635e6206c07c4d5008b87376e7cb
JuliaHsantana <contato.juliahsantana@gmail.com>
2024-03-15 01:03:47 -0300

    Initial commit

---
+++ desafio_react/desafio-react/src/componentes/Header/Header.jsx
@@ -0,0 +1,17 @@
+import "./Header.css"
+
+
+
+function Header(){
+
+    return(
+        <>
+        <div>
+        <h1>Mais um desafio completo ❤️</h1>
+        <img src="https://i.pinimg.com/originals/47/27/a7/4727a732cea56ca93a973b00fb264136.jpg" alt="lulu league of" />
+        </div>
+        </>
+        )
+}
+
+export default Header
